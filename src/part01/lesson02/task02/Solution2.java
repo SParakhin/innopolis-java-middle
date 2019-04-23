ee97042bbca3b044aaacb784622dd587f9eff50d
Sergey Parakhin <ormts@mail.ru>
2019-04-23 12:09:45 +0300

    Solution for lesson2 ver 3.0

--- src/part01/lesson02/task02/Solution2.java
+++ src/part01/lesson02/task02/Solution2.java
@@ -6,24 +6,30 @@
  * Предусмотреть что первоначальные числа могут быть отрицательные, в этом случае генерировать исключение.
  */
 package part01.lesson02.task02;
+
 import static java.lang.Math.*;
 
 public class Solution2 {
     public static void main(String[] args) {
 
-        int[] array = new int[1000];
+        int[] array = new int[100];
         for (int k = 0; k < array.length; k++) {
             array[k] = (int) (Math.random() * 2000 - 1000);
             try {
                 if (array[k] < 0) {
-                    throw new Exception("Отрицательное число");
+                    throw new Exception("Отрицательное число, корень не извлекаем" + " " + array[k]);
                 }
             } catch (Exception e) {
                 System.err.println(e.getMessage());
             }
-            int q = (int) sqrt(array[k]);
-            if (array[k] == q * q)
-                System.out.println(q + "*" + q + "=" + array[k]);
+            if (array[k] >= 0) {
+                int q = (int) sqrt(array[k]);
+                if (array[k] == q * q) {
+                    System.out.println("=======Найдено совпадение" + " " + q + "*" + q + "=" + array[k]);
+                } else {
+                    System.out.println(array[k]);
+                }
+            }
         }
     }
 }
