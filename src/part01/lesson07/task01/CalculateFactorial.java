b5d13dc217df9bda5d56e8b96314512b098599ee
Sergey Parakhin <ormts@mail.ru>
2019-05-16 14:28:15 +0300

    Solution for lesson 07 ver. 2.0

---
+++ src/part01/lesson07/task01/CalculateFactorial.java
@@ -0,0 +1,48 @@
+package part01.lesson07.task01;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class CalculateFactorial {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        long start = System.currentTimeMillis();
+        calcuateFactorial(100, BigInteger.ONE);
+        long end = System.currentTimeMillis();
+        long itog = end - start;
+        System.out.println("Время подсчета " + itog);
+    }
+
+    /**
+     * Метод расчета факториала.
+     *
+     * @param number
+     * @param answer
+     * @throws InterruptedException
+     * @throws ExecutionException
+     */
+    static void calcuateFactorial(int number, BigInteger answer) throws InterruptedException, ExecutionException {
+        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() - 1);
+        List<Future<BigInteger>> totalTaskResults = new ArrayList<>();
+        for (int start = 1; start <= number; start++) {
+            int end = start + 1;
+            Future<BigInteger> taskResult;
+            if (end > start) {
+                taskResult = executor.submit(new Multiply(start, 1));
+                Callable<String> task = () -> Thread.currentThread().getName();
+                Future result = executor.submit(task);
+                System.out.println(result.get());
+            } else {
+                taskResult = executor.submit(new Multiply(start, end));
+            }
+            totalTaskResults.add(taskResult);
+        }
+        for (Future<BigInteger> future : totalTaskResults) {
+            answer = answer.multiply(future.get());
+            executor.shutdown();
+        }
+        System.out.println("Factorial " + number + "=" + answer);
+    }
+}
---
+++ src/part01/lesson07/task01/Multiply.java
@@ -0,0 +1,22 @@
+package part01.lesson07.task01;
+
+import java.math.BigInteger;
+import java.util.concurrent.Callable;
+
+public class Multiply implements Callable<BigInteger> {
+    private int start;
+    private int end;
+
+
+    public Multiply(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public BigInteger call() {
+        BigInteger a = BigInteger.valueOf(start);
+        BigInteger b = BigInteger.valueOf(end);
+        return a.multiply(b);
+    }
+}
