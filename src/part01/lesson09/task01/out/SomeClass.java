20d4dda0fe997dcea6f3e6fe242741dd0f3ad0ab
SParakhin <ormts@mail.ru>
2019-05-18 13:25:32 +0300

    Solution for lesson 07 ver. 2.0

--- src/part01/lesson09/task01/out/SomeClass.java
+++
@@ -1,6 +0,0 @@
-package part01.lesson09.task01.out;
-import part01.lesson09.task01.interfaces.Worker;
-public class SomeClass implements Worker{
-public void doWork() {
-System.out.print("Hello");}
-}
