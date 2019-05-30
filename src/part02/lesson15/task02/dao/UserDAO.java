fdc604cca70e8a2199eb7775ba87c5bdebca4211
SParakhin <ormts@mail.ru>
2019-05-30 23:49:32 +0300

    Solution for lesson 15 ver. 2.0

---
+++ src/part02/lesson15/task02/dao/UserDAO.java
@@ -0,0 +1,125 @@
+package part02.lesson15.task02.dao;
+
+import java.sql.*;
+
+public class UserDAO {
+
+    private static final String SQL_INSERT_INTO_USER = "INSERT INTO user (name,birthday,login_id,city,email) VALUES (?,?,?,?,?) ";
+    private static final String SQL_SELECT_INTO_USER = "SELECT * FROM user WHERE login_id=? AND name=?";
+    private static final String SQL_SELECT_INTO_USER_ROLE = "INSERT INTO user_role (user_id,role_id) VALUES (?,?)";
+
+
+    /**
+     * Метод групповой записи данных(INSERT) в таблицу использованием batch процесс
+     *
+     * @param connection
+     * @throws SQLException
+     */
+
+    public static void addBatch(Connection connection) throws SQLException {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(SQL_INSERT_INTO_USER)) {
+            connection.setAutoCommit(false);
+            for (int i = 0; i < 10; i++) {
+                preparedStatement.setString(1, "Sergey" + i);
+                preparedStatement.setString(2, "09.01.1975" + i);
+                preparedStatement.setInt(3, 1);
+                preparedStatement.setString(4, "Orel" + i);
+                preparedStatement.setString(5, "ormts@mail.ru" + i);
+                preparedStatement.addBatch();
+            }
+            int[] i = preparedStatement.executeBatch();
+            System.out.println("Добавлено " + i.length + " записей в таблицу user");
+            connection.commit();
+            connection.setAutoCommit(true);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            connection.rollback();
+        }
+    }
+
+    /**
+     * Метод записи данных в таблице с использованием параметризированного запроса
+     *
+     * @param connection
+     * @throws SQLException
+     */
+    public static void addStatement(Connection connection) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement(SQL_INSERT_INTO_USER);
+        preparedStatement.setString(1, "Sergey");
+        preparedStatement.setString(2, "09.01.1975");
+        preparedStatement.setInt(3, 1);
+        preparedStatement.setString(4, "Orel");
+        preparedStatement.setString(5, "ormts@mail.ru");
+        preparedStatement.execute();
+        preparedStatement.close();
+    }
+
+    /**
+     * Метод  параметризированной выборки по login_ID и name одновременно
+     *
+     * @param connection
+     * @throws SQLException
+     */
+    public static void getQuery(Connection connection) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement(SQL_SELECT_INTO_USER);
+        preparedStatement.setInt(1, 1);
+        preparedStatement.setString(2, "Sergey");
+        ResultSet rs = preparedStatement.executeQuery();
+        int columns = rs.getMetaData().getColumnCount();
+        while (rs.next()) {
+            for (int i = 1; i <= columns; i++) {
+                System.out.print(rs.getString(i) + "\t");
+            }
+            System.out.println();
+        }
+        preparedStatement.close();
+    }
+
+    /**
+     * Метод ручного управления транзакциями
+     * Между sql операциями установить точку сохранения (SAVEPOINT A),
+     * намеренно ввести некорректные данные на последней операции, что бы транзакция
+     * откатилась к логической точке SAVEPOINT A
+     *
+     * @param connection
+     * @throws SQLException
+     */
+    public static void addManualTransaction(Connection connection) throws SQLException {
+        Savepoint savepoint = null;
+        connection.setAutoCommit(false);
+        try (PreparedStatement preparedStatement = connection.prepareStatement(SQL_INSERT_INTO_USER)) {
+            preparedStatement.setString(1, "Ivan");
+            preparedStatement.setString(2, "09.01.1990");
+            preparedStatement.setInt(3, 1);
+            preparedStatement.setString(4, "Orel");
+            preparedStatement.setString(5, "xxx@mail.ru");
+            preparedStatement.execute();
+            savepoint = connection.setSavepoint("А");
+            preparedStatement.setString(1, "Dima");
+            preparedStatement.setString(2, "09.12.1989");
+            preparedStatement.setString(3, "234132421341");//неверные параметры
+            preparedStatement.setString(4, "Moscow");
+            preparedStatement.setString(5, "yyy@mail.ru");
+            preparedStatement.execute();
+            connection.commit();
+        } catch (SQLException e) {
+            System.err.println("Ошибка транзакции. Добавлены данные до точки восстановления");
+            connection.rollback(savepoint);
+            connection.commit();
+
+        }
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(SQL_SELECT_INTO_USER_ROLE)) {
+            preparedStatement.setInt(1, 1);
+            preparedStatement.setInt(2, 2);
+            preparedStatement.addBatch();
+            int[] j = preparedStatement.executeBatch();
+            System.out.println("Добавлено " + j.length + " записей в таблицу user_role в режиме ручной транзакции ");
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            connection.rollback();
+        }
+        connection.setAutoCommit(true);
+    }
+}
