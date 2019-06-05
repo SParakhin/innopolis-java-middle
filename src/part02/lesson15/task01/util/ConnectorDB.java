4fbacec902aeb00acec8b140480d562900fd4741
Sergey Parakhin <ormts@mail.ru>
2019-06-05 15:16:07 +0300

    Solution for lesson 15 ver. 2.0

--- src/part02/lesson15/task01/util/ConnectorDB.java
+++ src/part02/lesson15/task01/util/ConnectorDB.java
@@ -1,13 +1,8 @@
-package part02.lesson15.task02.util;
+package part02.lesson15.task01.util;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 import java.util.ResourceBundle;
 
 public class ConnectorDB {
