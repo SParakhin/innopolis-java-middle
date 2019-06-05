8a76c616ad89f694aa4e01c8530f244b082143e6
Sergey Parakhin <ormts@mail.ru>
2019-06-05 15:19:59 +0300

    Solution for lesson 15 ver. 2.0

--- src/part02/lesson15/task03/lesson15.sql
+++ src/part02/lesson15/task03/lesson15.sql
@@ -14,11 +14,11 @@ CREATE TABLE `role` (
   `name` varchar(255) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB
+) ;
 
 CREATE TABLE `user_role` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) DEFAULT NULL,
   `role_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB
+) ;
